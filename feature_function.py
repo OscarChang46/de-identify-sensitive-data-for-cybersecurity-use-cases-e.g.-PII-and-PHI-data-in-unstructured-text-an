dc4d9b7b9eeb28976db88969a4e4253e50184d93
OscarChang46 <1194148004@qq.com>
2021-11-06 18:05:46 -0700

    feature function

--- feature_function.py
+++ feature_function.py
@@ -1,14 +1,25 @@
 import numpy as np
 
-class StausFF():
+class StausFF(object):
     def __init__(self, c):
         self.c = c
     
     def __call__(self, y_, y, X, i):
         return 1 if y == self.c else 0
 
-class ObsFF():
-    pass
+class ObsFF(object):
+    def __init__(self, j ,c, d):
+        self.j, self.c, self.d = j, c, d
 
-class TransFF():
-    pass
+    def __call__(self, y_, y, X, i):
+        return 1 if y==self.c and X[i, self.j] == self.d else 0
+
+class TransFF(object):
+    '''
+    indicate whether y_{i} == c, y_{i-1} == d
+    '''
+    def __init__(self, c, d):
+        self.c, self.d = c, d
+    
+    def __call__(self, y_, y, X, i):
+        return 1 if y_==self.d and y == self.c else 0
