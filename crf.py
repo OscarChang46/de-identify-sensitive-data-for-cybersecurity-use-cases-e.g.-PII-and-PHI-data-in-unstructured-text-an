1052586093788cf45f9e8d1f9253f8585177d32d
OscarChang46 <1194148004@qq.com>
2021-12-01 12:58:51 -0800

    CRF

--- crf.py
+++ crf.py
@@ -1,1 +1,29 @@
 import numpy as np
+from scipy.optimize import fmin_l_bfgs_b
+
+import time 
+import json
+import datetime
+
+# input: feature
+# output: probability
+
+
+# generic form
+def probability_function():
+    pass
+
+
+def _generate_potential_table(params, num_labels, feature_set, X, inference=True):
+    '''
+    Generate a potential table using given obeservations.
+    * potential_table[t][pre_y, y]
+        := exp(inner_product(params, feature_vector(pre_y, y, X, t)))
+    '''
+    pass
+
+
+
+
+def _forward_backward():
+    pass
